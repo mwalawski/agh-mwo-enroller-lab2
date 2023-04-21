b5f1c4f596e4275abb51b2ab86ecf960b58ba120
mwalawski <michal.walawski@awesome-ind.com>
2023-04-21 22:09:09 +0200

    add authentication

--- src/main/java/com/company/enroller/security/WebSecurity.java
+++ src/main/java/com/company/enroller/security/WebSecurity.java
@@ -1,9 +1,15 @@
 package com.company.enroller.security;
 
+import com.company.enroller.App;
+import com.company.enroller.persistence.ParticipantService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @EnableWebSecurity
 public class WebSecurity extends WebSecurityConfigurerAdapter {
@@ -13,6 +19,31 @@ public class WebSecurity extends WebSecurityConfigurerAdapter {
                 .authorizeRequests()
                 .anyRequest().permitAll()
                 .and()
+                .addFilterBefore(
+                        new JWTAuthenticationFilter(
+                                authenticationManager(),
+                                secret,
+                                issuer,
+                                tokenExpiration),
+                        UsernamePasswordAuthenticationFilter.class)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+    }
+
+    @Autowired
+    private ParticipantProvider participantProvider;
+    @Autowired
+    private App app;
+
+    @Value("${security.secret}")
+    String secret;
+    @Value("${security.issuer}")
+    String issuer;
+    @Value("${security.token_expiration_in_seconds}")
+    int tokenExpiration;
+
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(participantProvider)
+                .passwordEncoder(app.passwordEncoder());
     }
 }
